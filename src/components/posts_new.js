b83122762ae973e75a9e37fd9e4a6008052e8722
Nitin <nitin@smove.sg>
2018-04-15 17:23:17 +0800

    Add red warning for validation errors, cancel btn

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
   render() {
@@ -9,21 +10,21 @@ class PostsNew extends Component {
     return (
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create new Post</h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
           <div className="text-help">
             { title.touched ? title.error: '' }
           </div>
         </div>
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
           <div className="text-help">
             { categories.touched ? categories.error: '' }
           </div>
         </div>
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea className="form-control" {...content} />
           <div className="text-help">
@@ -32,6 +33,7 @@ class PostsNew extends Component {
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
